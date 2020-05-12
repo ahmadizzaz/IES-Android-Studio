65437460b5fef80ea27509e01a6cafcab7210dbb
unknown <izzaz.iqbal@gmail.com>
2020-05-12 14:04:33 +0800

    New commmit

--- Week2/app/src/main/java/com/izzaz/week2/model/DogModel.kt
+++
@@ -1,3 +0,0 @@
-package com.izzaz.week2.model
-
-data class DogModel(var url: String,var name: String, var breed: String)
--- Week2/app/src/main/java/com/izzaz/week2/model/FoodItem.kt
+++
@@ -1,4 +0,0 @@
-package com.izzaz.week2.model
-
-data class FoodItem(var food: String)
-
